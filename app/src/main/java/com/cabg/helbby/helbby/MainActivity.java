c60968baf5e178944a65601564baab20b4fb5be2
Andres Gonzalez <Andres9321Gonzalez@gmail.com>
2017-09-05 12:54:52 -0500

    1. Login y registro

--- app/src/main/java/com/cabg/helbby/helbby/MainActivity.java
+++ app/src/main/java/com/cabg/helbby/helbby/MainActivity.java
@@ -6,7 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -14,19 +21,30 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String IP = "https://helbby.000webhostapp.com/PHPhelbby/Login_GETEMAIL.php?email=";
     TextView textViewHelbby;
     Button registrarse, recuperar, ingresar;
     LoginButton loginButton1;
     CallbackManager callbackManager;
+    EditText etEmail, etPassword;
+    RequestQueue mRequest;
+    VolleyRP volleyRP;
+    String EMAIL = "";
+    String PASSWORD = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        volleyRP = VolleyRP.getInstance(this);
+        mRequest = volleyRP.getRequestQueue();
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
@@ -44,13 +62,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        etEmail = (EditText) findViewById(R.id.emailLogin);
+        etPassword = (EditText) findViewById(R.id.passwordLogin);
+
+        ingresar = (Button) findViewById(R.id.ingresar);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verificarLogin(etEmail.getText().toString().toLowerCase(),
+                        etPassword.getText().toString().toLowerCase());
+            }
+        });
+
 
         // Cambiar fuente texview helbby //
         textViewHelbby = (TextView) findViewById(R.id.helbby);
         Typeface fuente = Typeface.createFromAsset(getAssets(), "fonts/Anydore.otf");
         textViewHelbby.setTypeface(fuente);
-
-
 
         registrarse = (Button) findViewById(R.id.registro);
         registrarse.setOnClickListener(new View.OnClickListener() {
@@ -72,15 +100,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ingresar = (Button) findViewById(R.id.ingresar);
-        ingresar.setOnClickListener(new View.OnClickListener() {
+
+    }
+
+    public void solicitudJSON(String URL){
+        JsonObjectRequest solicitud = new JsonObjectRequest(URL, null, new Response.Listener<JSONObject>(){
             @Override
-            public void onClick(View view) {
-                Intent intentIngresar = new Intent(MainActivity. this, HelbbyActivity.class);
-                startActivity(intentIngresar);
+            public void onResponse(JSONObject datos) {verificarPassword(datos);
+            }
+        },new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Ocurrio un error", Toast.LENGTH_SHORT).show();
             }
         });
+        VolleyRP.addToQueue(solicitud, mRequest, this, volleyRP);
     }
+
+    public void verificarLogin(String email, String password){
+        EMAIL =  email;
+        PASSWORD = password;
+        solicitudJSON(IP + email);
+
+    }
+
+    public void verificarPassword(JSONObject datos){
+        try {
+            String estado = datos.getString("resultado");
+            if (estado.equals("ConsultaCorrecta")){
+
+                JSONObject jsonDatos = new JSONObject(datos.getString("datos"));
+                String email = jsonDatos.getString("email");
+                String password = jsonDatos.getString("password");
+
+                if (email.equals(EMAIL) && password.equals(PASSWORD)){
+                    Toast.makeText(this, "Usted se ha logeado correctamente",Toast.LENGTH_SHORT).show();
+                    Intent entrar =  new Intent(this, HelbbyActivity.class);
+                    startActivity(entrar);
+                }else {
+                    Toast.makeText(this, "La contraseña es incorrecta",Toast.LENGTH_SHORT).show();
+                }
+
+            }else
+                Toast.makeText(this, estado , Toast.LENGTH_SHORT).show();
+
+        } catch (JSONException e) {
+
+        }
+    }
+
 
     private void goMainScreen() {
         Intent intent = new Intent(this, HelbbyActivity.class);
--- app/src/main/java/com/cabg/helbby/helbby/RegistroActivity.java
+++ app/src/main/java/com/cabg/helbby/helbby/RegistroActivity.java
@@ -3,7 +3,16 @@ package com.cabg.helbby.helbby;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -11,15 +20,30 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+
 public class RegistroActivity extends AppCompatActivity {
 
+    private static final String IP_REGISTRAR = "https://helbby.000webhostapp.com/PHPhelbby/Registro_INSERT.php";
     LoginButton loginButton2;
+    EditText etNombre, etEmail, etPass;
     CallbackManager callbackManager;
+    Button buttonRegistrarse;
+    RequestQueue mRequest;
+    VolleyRP volleyRP;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        volleyRP = VolleyRP.getInstance(this);
+        mRequest = volleyRP.getRequestQueue();
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
@@ -36,7 +60,67 @@ public class RegistroActivity extends AppCompatActivity {
             public void onError(FacebookException error) {
             }
         });
+
+        etNombre = (EditText) findViewById(R.id.nombreRegistro);
+        etEmail = (EditText) findViewById(R.id.EmailRegistro);
+        etPass = (EditText) findViewById(R.id.contraseñaRegistro);
+        buttonRegistrarse = (Button) findViewById(R.id.registrarse);
+        buttonRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrarWebService(getStringET(etNombre),getStringET(etEmail),getStringET(etPass));
+            }
+        });
+
     }
+
+    private void registrarWebService(String nombre,String email,String password){
+
+        if(!nombre.isEmpty() &&
+                !email.isEmpty() &&
+                !password.isEmpty()) {
+
+            HashMap<String, String> hashMapToken = new HashMap<>();
+            hashMapToken.put("nombre", nombre);
+            hashMapToken.put("email", email);
+            hashMapToken.put("password", password);
+
+
+            JsonObjectRequest solicitud = new JsonObjectRequest(Request.Method.POST, IP_REGISTRAR,
+                    new JSONObject(hashMapToken),
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject datos) {
+                            try {
+                                String estado = datos.getString("resultado");
+                                if (estado.equalsIgnoreCase("El usuario se registro correctamente")) {
+                                    Toast.makeText(RegistroActivity.this, estado, Toast.LENGTH_SHORT).show();
+                                    Intent entrar =  new Intent(RegistroActivity.this, HelbbyActivity.class);
+                                    startActivity(entrar);
+                                } else {
+                                    Toast.makeText(RegistroActivity.this, estado, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                Toast.makeText(RegistroActivity.this, "No se pudo registrar", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(RegistroActivity.this, "No se pudo registrar", Toast.LENGTH_SHORT).show();
+                }
+            });
+            VolleyRP.addToQueue(solicitud, mRequest, this, volleyRP);
+        }else{
+            Toast.makeText(this, "Por favor llene todo los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private String getStringET (EditText editTextRegistro){
+        return editTextRegistro.getText().toString();
+    }
+
 
     private void goMainScreen() {
         Intent intent = new Intent(this, HelbbyActivity.class);
